39511aab298ae121a0d77781ef8f8851cb5494f7
Scott Olesen <ulp7@cdc.gov>
2024-06-03 13:00:44 -0400

    working: updating params & sim

--- scripts/run_simulations.R
+++ scripts/run_simulations.R
@@ -2,75 +2,33 @@
 
 library(tidyverse)
 
-seed <- 13
-# number of simulations
-n_reps <- 10000
-out_path <- "output/simulations"
+output_dir <- "output"
 
-future::plan(future::multicore)
+# if multicore parallel processing is available, use it
+if (future::supportsMulticore()) {
+  future::plan(future::multicore)
+}
 
-# Read in parameters ----------------------------------------------------------
-config <- seirMeasles::read_params(
+# Run simulations ----------------------------------------------------------
+# clear existing output
+parameters_path <- file.path(output_dir, "parameters.rds")
+results_path <- file.path(output_dir, "results")
+
+unlink(parameters_path)
+unlink(results_path, recursive = TRUE)
+
+# read in experiment specification
+spec <- simWrapper::read_spec(
   system.file("params.yaml", package = "seirMeasles", mustWork = TRUE)
 )
 
-# setup parameter variations
-parameter_grid <- expand.grid(c(
-  list(sim = 1:n_reps),
-  config$params
-))
-
-base_import_days <- config$history$import_days
-
-base_vaccine_doses <- config$history$vaccine_doses
-base_vaccine_days <- config$history$vaccine_days +
-  config$history$immunity_onset_delay
-
-# Scenario running functions --------------------------------------------------
-
-report <- function(msg) {
-  cat(paste(msg, date(), "", sep = "\n"))
-}
-
-#' Write day as date
-#'
-#' @param dt date
-#'
-#' @return string in form `"March_08"`
-format_day <- function(x) {
-  format(
-    seirMeasles::day_to_date(x, start_date = config$history$start_date),
-    "%B_%d"
-  )
-}
-
-#' Run and write a scenario to disk
-#'
-#' @param scenario name
-#' @param ... Additional parameters passed to
-#'   [seirMeasles::generate_inputs()]
-write_scenario <- function(scenario, ...) {
-  report(scenario)
-
-  seirMeasles::simulate_grid(
-    parameter_grid,
-    manual_inputs = seirMeasles::generate_inputs(
-      import_days = base_import_days,
-      ...
-    ),
-    seed = seed
-  ) |>
-    mutate(
-      scenario = !!scenario,
-      int_day = case_when(
-        is.na(int_day) ~ "No_intervention",
-        TRUE ~ format_day(int_day)
-      )
-    ) |>
-    arrange(scenario, int_day, r_0) |>
-    group_by(scenario, int_day, r_0) |>
-    arrow::write_dataset(out_path)
-}
+# run experiment & cache results
+simWrapper::cache_experiment(
+  fun = seirMeasles::simulate,
+  spec = spec,
+  parameters_path = file.path(output_dir, "parameters.rds"),
+  results_path = file.path(output_dir, "results")
+)
 
 # Run scenarios ---------------------------------------------------------------
 
