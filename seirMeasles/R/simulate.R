89fe9b9711770fab04efffd5a98fc8dafb66bc0d
Scott Olesen <ulp7@cdc.gov>
2024-06-03 13:35:06 -0400

    Update params.yaml for new "baseline" format

--- seirMeasles/R/simulate.R
+++ seirMeasles/R/simulate.R
@@ -115,16 +115,14 @@ do_vaccinations <- function(x, doses, ve, ve_infant, prop_vax_to_se) {
   x
 }
 
-#' Run one simulation
+#' Clean parameters list
 #'
-#' @param params named list of parameters
-#' @param ... additional arguments passed to [seirMeasles::ssa()]
+#' The parameters that are read in from `params.yaml` needs a few tweaks before
+#' they can get passed into the main simulation function
 #'
-#' @return tibble
-#'
-#' @export
-simulate <- function(params, ...) {
-  # ensure parameters are normalized
+#' @param params named list; parameter set
+#' @return parameter set
+clean_parameters <- function(params) {
   start_date <- lubridate::ymd(params$start_date)
 
   params <- purrr::modify_at(
@@ -134,6 +132,34 @@ simulate <- function(params, ...) {
   )
 
   stopifnot(length(params$vaccine_days) == length(params$vaccine_doses))
+
+  # for annoying reasons, the YAML must have NAs, but `generate_inputs()`
+  # expects NULL, so we just swap between them here
+  params <- purrr::modify_at(
+    params,
+    c("vaccine_days", "vaccine_doses"),
+    function(x) {
+      if (length(x) == 1 && is.na(x)) {
+        NULL
+      } else {
+        x
+      }
+    }
+  )
+
+  params
+}
+
+#' Run one simulation
+#'
+#' @param params named list of parameters
+#' @param ... additional arguments passed to [seirMeasles::ssa()]
+#'
+#' @return tibble
+#'
+#' @export
+simulate <- function(params, ...) {
+  params <- clean_parameters(params)
 
   # generate the "manual" inputs
   manual_inputs <- generate_inputs(
